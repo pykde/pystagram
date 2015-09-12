6d2e253dca84cf64f58b3429715a43bb1a568eb3
Kim Dong Eun <burningfist@naver.com>
2015-09-12 17:04:16 +0900

    setting_prod 수정

--- pystagram/settings_prod.py
+++ pystagram/settings_prod.py
@@ -9,10 +9,10 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES ={
     'default': {
-        'ENGINE: 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':'pystagram_db',
         'USER': 'ubuntu',
-        'PASSWORD: 'mypassword',
+        'PASSWORD': 'mypassword',
         'HOST':'127.0.0.1'
     },
 }
